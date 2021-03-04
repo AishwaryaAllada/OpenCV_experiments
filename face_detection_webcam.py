a0b5ca21b33f15375f19cf95821843beae57851d
Allada Aishwarya Krishna <50237215+AishwaryaAllada@users.noreply.github.com>
2021-03-04 15:17:27 -0500

    Add files via upload

---
+++ face_detection_webcam.py
@@ -0,0 +1,40 @@
+import cv2
+import sys
+
+# #load the cascade
+
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+#     # Capture frame-by-frame
+    ret, frame = video_capture.read()#read() reads one frame  and returns - 1.actual video frame read ;2. a return code (tells us if the video is over if we are reading from a file)
+
+
+# 	#the code below converts frames to greyscale and the code below we search for faces
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
